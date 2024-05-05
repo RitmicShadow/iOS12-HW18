b7be2f54c4e197f4974c2325b8ec165a483ea7f5
DenisMayshev <denmay13@mail.ru>
2024-05-05 16:59:44 +0400

    Complete

--- iOS12-HW18.playground/Contents.swift
+++ iOS12-HW18.playground/Contents.swift
@@ -1,4 +1,6 @@
 import Foundation
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 public struct Chip {
     public enum ChipType: UInt32 {
@@ -23,4 +25,73 @@ public struct Chip {
     }
 }
 
+class InOutStorage {
+    private var chips: [Chip] = []
 
+    func put(_ chip: Chip) {
+        chips.append(chip)
+    }
+
+    func get() -> Chip? {
+        if chips.isEmpty {
+            return nil
+        } else {
+            return chips.removeLast()
+        }
+    }
+
+    var isEmpty: Bool {
+        return chips.isEmpty
+    }
+}
+
+class GeneratorThread: Thread {
+    var storage: InOutStorage
+
+    init(storage: InOutStorage) {
+        self.storage = storage
+    }
+
+    override func main() {
+        print("GeneratorThread Начинает работу")
+        for _ in 0..<10 {
+            GeneratorThread.sleep(forTimeInterval: 2)
+            let newChip = make() 
+            print(newChip.chipType)
+            storage.put(newChip)
+            print("Chip добавлен")
+        }
+        print("GeneratorThread Закончил выполнение")
+        print("WorkerThread Закончил выполнение")
+    }
+
+    func make() -> Chip {
+        return Chip(chipType: Chip.ChipType(rawValue: UInt32(arc4random_uniform(3) + 1))!)
+    }
+}
+
+class WorkerThread: Thread {
+    var storage: InOutStorage
+
+    init(storage: InOutStorage) {
+        self.storage = storage
+    }
+
+    override func main() {
+        print("WorkerThread Начинает выполнение")
+        while !storage.isEmpty {
+            if let chip = storage.get() {
+                chip.sodering()
+            } else {
+                WorkerThread.sleep(forTimeInterval: 1)
+            }
+        }
+    }
+}
+
+let storage = InOutStorage()
+let generatorThread = GeneratorThread(storage: storage)
+let workerThread = WorkerThread(storage: storage)
+
+generatorThread.start()
+workerThread.start()
